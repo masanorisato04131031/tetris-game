9d1f235797fa0a414cae79daa0928b2b5500d0c3
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-05-10 14:35:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
+const canvas = document.getElementById('tetris');
 
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function arenaSweep() {
+  let rowCount = 1;
+
+  outer: for (let y = arena.length - 1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    ++y;
+
+    player.score += rowCount * 100;
+    player.lines += 1;
+    rowCount *= 2;
+  }
+}
+
+function updateScore() {
+  scoreElement.innerText = player.score;
+  linesElement.innerText = player.lines;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+let lastTime = 0;
+
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+
+  lastTime = time;
+  dropCounter += deltaTime;
+
+  if (dropCounter > dropInterval) {
+    playerDrop();
+  }
+
+  draw();
+  requestAnimationFrame(update);
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowLeft') {
+    playerMove(-1);
+  } else if (event.key === 'ArrowRight') {
+    playerMove(1);
+  } else if (event.key === 'ArrowDown') {
+    playerDrop();
+  } else if (event.key === 'ArrowUp') {
+    playerRotate(1);
+  } else if (event.code === 'Space') {
+    hardDrop();
+  }
+});
+
+playerReset();
+updateScore();
+update();
